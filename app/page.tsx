348d3423f6a4f682f0d0a17f5da46844b1ebf1a4
Aashutoshbista <aashutoshbista717@gmail.com>
2026-06-18 13:44:13 +0545

    File structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import products from "./data/products";
-import ProductCard from "./components/ProductCard";
 
+import ProductGrid from "@/component/ProductGrid";
 export default function Home() {
   return (
-    <main className="grid gap-8 p-10 md:grid-cols-2 lg:grid-cols-3">
-      {products.map((product) => (
-        <ProductCard
-          key={product.id}
-          image={product.image}
-          title={product.title}
-          description={product.description}
-          price={product.price}
-        />
-      ))}
-    </main>
+    <div>
+      <ProductGrid />
+    </div>
   );
 }
